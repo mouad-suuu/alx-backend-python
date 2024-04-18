6ad868bd54d9c69a8809e3dafc83cbd84292dc8e
mouad mennioui <moad.menniui@gmail.com>
2024-04-18 15:30:11 +0100

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-'''a function add that takes a float a and 
+'''a function add that takes a float a and
 a float b as arguments and returns their sum as a float.
 '''
 
 
-def add(a:float, b:float) ->float:
+def add(a: float, b: float) -> float:
     '''returns the sum of two floats'''
     return a + b
