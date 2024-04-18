5d095eed4c49a74ec66f825ae800c94a276978d0
mouad mennioui <moad.menniui@gmail.com>
2024-04-18 16:20:59 +0100

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -8,7 +8,8 @@ should be annotated as a float.
 from typing import Union, Tuple
 
 
-def to_kv(K: str, v: Union[int, float]) -> Tuple[str, float]:
-    ''' a type-annotated function to_kv
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''Converts a key and its value to a tuple of the key and
+    the square of its value.
     '''
-    return (K, float(v**2))
+    return (k, float(v**2))
