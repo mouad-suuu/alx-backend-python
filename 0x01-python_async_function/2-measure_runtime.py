417edf0e2c69d985ec69a30e89be3563d5b95d28
mouad mennioui <moad.menniui@gmail.com>
2024-04-22 19:41:30 +0100

    Create 2-measure_runtime.py

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''Create a measure_time function
+'''
+import asyncio
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    '''return the time of execution
+    '''
+    start = time.perf_counter()
+    await wait_n(n, max_delay)
+    end = time.perf_counter()
+    total_time = end - start
+    return total_time/n
