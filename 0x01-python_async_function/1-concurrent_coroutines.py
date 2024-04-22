ae31ff37b9d83cccfe7c37ea7decee8388fec09e
mouad mennioui <moad.menniui@gmail.com>
2024-04-22 19:03:13 +0100

    Create 1-concurrent_coroutines.py

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''write an async routine called wait_n
+'''
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''return the list of all the delays
+    '''
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return delays
