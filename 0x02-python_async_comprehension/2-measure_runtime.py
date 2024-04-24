2f13bab112c585da0cfe383778bb62f65757886d
mouad mennioui <moad.menniui@gmail.com>
2024-04-24 02:32:47 +0100

    Update 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
-'''Task 1's module.
+'''Task 2's module.
 '''
-from typing import List
+import asyncio
+import time
 from importlib import import_module as using
 
 
-async_generator = using('0-async_generator').async_generator
+async_comprehension = using('1-async_comprehension').async_comprehension
 
 
-async def async_comprehension() -> List[float]:
-    '''Creates a list of 10 numbers from a 10-number generator.
+async def measure_runtime() -> float:
+    '''Executes async_comprehension 4 times and measures the
+    total execution time.
     '''
-    return [num async for num in async_generator()]
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time() - start_time
